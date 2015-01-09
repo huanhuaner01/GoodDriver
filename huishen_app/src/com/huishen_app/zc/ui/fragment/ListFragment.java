8e8d5b325fd615f87fa57ab59ab94429d458e483
huanhuaner01 <916243763@qq.com>
2015-01-09 09:54:15 +0800

    添加地图展示地理位置功能

---
+++ huishen_app/src/com/huishen_app/zc/ui/fragment/ListFragment.java
@@ -0,0 +1,85 @@
+package com.huishen_app.zc.ui.fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.huishen_app.all.mywidget.NoScrollListView;
+import com.huishen_app.zc.ui.R;
+import com.huishen_app.zc.ui.base.BaseActivity;
+import com.huishen_app.zc.ui.fragment.ListFragment.ListFragmentAdapter;
+
+/**
+ * 具有文字，列表和标题的Fragment(通用)
+ * @author zhanghuan
+ *
+ */
+@SuppressLint("InflateParams") public class ListFragment extends BaseFragment {
+	private View RootView ;  //根组件
+	private TextView title ; //标题
+	private TextView des ; //简介
+	private Button back  ; //返回键
+	private NoScrollListView list ; //列表
+	private ListFragmentAdapter fragmentAdapter ; //fragment适配器
+	private String result  ,titleStr;
+	private int url ;
+	
+	
+	public ListFragment(BaseActivity father ,String title, int url ,ListFragmentAdapter fragmentApdater) {
+		super(father);
+		this.fragmentAdapter = fragmentApdater;
+		this.titleStr = title ;
+		this.url = url ;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		    
+		try{
+			RootView = inflater.inflate(R.layout.fragment_list, null);
+			registView();
+			initView();
+		  } catch (Exception e) {
+		}
+		return RootView;
+	}
+
+	private void registView() {
+		title = (TextView)RootView.findViewById(R.id.header_title) ;
+		des = (TextView)RootView.findViewById(R.id.f_list_content) ;
+		list = (NoScrollListView)RootView.findViewById(R.id.f_list_list);
+		back = (Button)RootView.findViewById(R.id.head_back) ;
+	}
+    
+	private void initView() {
+	   this.title.setText(titleStr) ;
+	   getWebData();
+	   fragmentAdapter.setDes(result, des);
+	   fragmentAdapter.setList(result, list);
+	}
+	
+	private void getWebData(){
+		
+	}
+	
+	public void setBack(View.OnClickListener listener){
+		if(listener != null){
+		this.back.setOnClickListener(listener);
+		}
+	}
+	/**
+	 * listFragment 的接口函数
+	 * @author zhanghuan
+	 * 
+	 */
+	public interface ListFragmentAdapter{
+		void setDes(String result ,TextView tv);
+		void setList(String result ,NoScrollListView list);
+	}
+    
+}
