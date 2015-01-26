fb6d530ea3dfded3f7fa08dab9c02063c91f369b
huanhuaner01 <916243763@qq.com>
2015-01-26 17:30:00 +0800

    添加教练添加查找的弹出框

--- huishen_app/src/com/huishen_app/zc/ui/JLListActivity.java
+++ huishen_app/src/com/huishen_app/zc/ui/JLListActivity.java
@@ -8,17 +8,22 @@ import java.util.Map;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import com.huishen_app.zc.ui.R;
 import com.huishen_app.zc.ui.adapter.JL_Search_Adapter;
-import com.huishen_app.zc.ui.adapter.JX_Search_Adapter;
 import com.huishen_app.zc.ui.base.BaseActivity;
 
 @SuppressLint("ValidFragment")
@@ -27,7 +32,10 @@ public class JLListActivity extends BaseActivity implements SwipeRefreshLayout.O
 	private static final int REFRESH_COMPLETE = 0X110;
 	private SwipeRefreshLayout mSwipeLayout;
 	private TextView title ;
-	
+	private PopupWindow conditionPopWindow;// 条件搜索浮动窗口
+	private GridView popGrid ; 
+	private ArrayList<HashMap<String , Object>> popdata ; //popwindow的数据
+	private 
 	private List<Map<String, Object>> listview_date;
 	private JL_Search_Adapter adapter;
 
@@ -57,7 +65,7 @@ public class JLListActivity extends BaseActivity implements SwipeRefreshLayout.O
 		int[] to = {};
 		listview_date = new ArrayList<Map<String, Object>>();
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("photo","http://preview.quanjing.com/mf025/mf700-00609711.jpg");
+		map.put("photo","http://picture.51efu.cn/images/201104/thumb_img/2507_thumb_P_1304045401776.jpg");
 		listview_date.add(map);
 		map = new HashMap<String, Object>();
 		map.put("photo","http://img5.imgtn.bdimg.com/it/u=3793805207,3272353858&fm=21&gp=0.jpg");
@@ -66,7 +74,7 @@ public class JLListActivity extends BaseActivity implements SwipeRefreshLayout.O
 		map.put("photo","http://pic1.win4000.com/pic/1/ac/1fb0489694.jpg");
 		listview_date.add(map);
 		map = new HashMap<String, Object>();
-		map.put("photo","http://preview.quanjing.com/mf025/mf700-00609710.jpg");
+		map.put("photo","http://www.tbwfy.com/tufyL3R1ZnlpbWcwMi50YW9iYW9jZG4uY29tL2Jhby91cGxvYWRlZC9pMi8xNzcwNDAyOTEyMDc2ODEwNC9UMTNVd0JYbFJiWFhYWFhYWFhfISEwLWl0ZW1fcGljLmpwZw==.jpg");
 		listview_date.add(map);
 		map = new HashMap<String, Object>();
 		map.put("photo","http://preview.quanjing.com/mf025/mf700-00609701.jpg");
@@ -135,5 +143,51 @@ public class JLListActivity extends BaseActivity implements SwipeRefreshLayout.O
 		
 		mHandler.sendEmptyMessageDelayed(REFRESH_COMPLETE, 2000);
 
+	}
+	
+	/**
+	 * 条件搜索
+	 * @param v
+	 */
+	public void conditionSearch(View v){
+
+	
+		switch(v.getId()){
+		case R.id.jl_list_condition_location:
+		
+			conditionPopWindow.showAsDropDown(v);
+			break ;
+		case R.id.jl_list_condition_year:
+			conditionPopWindow.showAsDropDown(v);
+			break ;
+		case R.id.jl_list_condition_sex :
+			break ;
+		case R.id.jl_list_condition_more:
+			break ;
+	    default :
+				if(conditionPopWindow.isShowing()){
+				conditionPopWindow.dismiss() ;
+				}
+		}
+	}
+	/**
+	 * 初始化
+	 */
+	private void setPopWindow(){
+		View layout = LayoutInflater.from(this).inflate(R.layout.popwindow_ly, null); 
+		GridView grid = (GridView)layout.findViewById(R.id.grid);
+		popdata = new ArrayList<HashMap<String ,Object>>();
+		for(int i = 0 ;i< 10 ;i++){
+			HashMap<String , Object> map = new HashMap<String , Object>();
+			popdata.add(map) ;
+		}
+		adapter = new SimpleAdapter(this,popdata ,R.layout.popwindow_item ,new String[]{} ,new int[]{});
+		grid.setAdapter(adapter) ;
+		conditionPopWindow = new PopupWindow(layout,LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+		conditionPopWindow.setFocusable(true);// 加上这个popupwindow中的ListView才可以接收点击事件
+
+		// 控制popupwindow点击屏幕其他地方消失
+		conditionPopWindow.setBackgroundDrawable(new BitmapDrawable());// 设置背景图片，不能在布局中设置，要通过代码来设置
+		conditionPopWindow.setOutsideTouchable(true);// 触摸popupwindow外部，popupwindow消失。这个要求你的popupwindow要有背景图片才可以成功，如上
 	}
 }
---
+++ huishen_app/src/com/huishen_app/zc/ui/adapter/JlListPopGridAdapter.java
@@ -0,0 +1,33 @@
+package com.huishen_app.zc.ui.adapter;
+
+import java.util.List;
+import java.util.Map;
+
+import com.huishen_app.zc.ui.R;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class JlListPopGridAdapter extends SimpleAdapter {
+    private List<Map<String ,Object>> data ;
+	public JlListPopGridAdapter(Context context,
+			List<Map<String ,Object>> data, int resource, String[] from,
+			int[] to) {
+		super(context, data, resource, from, to);
+		this.data = data ;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = super.getView(position, convertView, parent);
+		if(Integer.parseInt(data.get(position).get("status").toString()) == 1){
+			TextView tv= (TextView)view.findViewById(R.id.jl_pop_itemtv) ;
+			tv.setSelected(true) ;
+		}
+		return view ;
+	}
+    
+}
